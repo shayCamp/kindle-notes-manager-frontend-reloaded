4e60bae0005103cbfec46dec142a7b2c8aaf15fe
Shay <shayc.campbell@gmail.com>
2022-07-03 22:47:31 +0100

    Creating Board

---
+++ src/Components/LandingComponents/AnalyticCards.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../Styling/AnalyticCards.scss';
+
+const AnalyticCards = ({ ...props }) => {
+    console.log(props);
+    return (
+        <div className="card1">
+            <h1></h1>
+        </div>
+    );
+};
+
+export default AnalyticCards;
---
+++ src/Components/LandingComponents/StreakBoard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../Styling/StreakBoard.scss';
+
+// interface StreakBoardProps {}
+
+const StreakBoard = ({ ...props }) => {
+    console.log(props);
+    return <div className="boardContainer"></div>;
+};
+
+export default StreakBoard;
