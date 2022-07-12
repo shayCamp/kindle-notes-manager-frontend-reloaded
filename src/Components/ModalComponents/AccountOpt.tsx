1eb13fee55d2a412a2a6abed3ae85d625ca4a07c
Shay <shayc.campbell@gmail.com>
2022-07-13 00:31:54 +0100

    MyAccount + Appearance Setting

--- src/Components/ModalComponents/AccountOpt.tsx
+++ src/Components/ModalComponents/AccountOpt.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../Styling/AccountOpt.scss';
+import { BsFillPencilFill } from 'react-icons/bs';
 
 // interface MyAccountProps {}
 
@@ -7,7 +8,22 @@ const AccountOpt = ({ ...props }) => {
     console.log(props);
     return (
         <div className="AccountOpt-page">
-            <h1>MyAccount</h1>
+            <div className="circle">
+                <div className="circle__edit">
+                    <BsFillPencilFill id="edit-icon" />
+                </div>
+            </div>
+            <div className="info-block"></div>
+            <div className="linked-block"></div>
+            <div
+                className="signOut-block"
+                onClick={() => {
+                    sessionStorage.clear();
+                    history.go(0);
+                }}
+            >
+                <p>Sign Out</p>
+            </div>
         </div>
     );
 };
